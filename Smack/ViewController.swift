f99dc63690d25bac4f3f7f62bcad2eae84b31a73
Rohit <rohitkumarrsoni1231@gmail.com>
2021-11-01 13:52:43 +0500

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by MacBook on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
