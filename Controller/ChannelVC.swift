7675cd6ec31e15c8211581760bfbb2de19f4a145
Rohit <rohitkumarrsoni1231@gmail.com>
2021-11-01 18:54:11 +0500

    navigation drawer UI Complete

--- Controller/ChannelVC.swift
+++ Controller/ChannelVC.swift
@@ -12,7 +12,7 @@ class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width -   60
     
     }
     
