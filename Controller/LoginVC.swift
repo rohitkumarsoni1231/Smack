46c54953a1a32fbdd2daf1ca6612f769912b3eed
Rohit <rohitkumarrsoni1231@gmail.com>
2021-11-02 14:49:55 +0500

    create account UI

--- Controller/LoginVC.swift
+++ Controller/LoginVC.swift
@@ -21,5 +21,9 @@ class LoginVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+    }
+    
 }
--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -11,3 +11,6 @@ import Foundation
 
 
 let TO_LOGIN = "toLogin"
+
+let TO_CREATE_ACCOUNT = "toCreateAccount"
+let UNWIND = "unwindToChannel"
